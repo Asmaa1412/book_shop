5e9bf9a9693239da8e799c37019c6361d7170181
Asmaa1412 <ayyoubasmaa2@gmail.com>
2023-08-26 19:01:59 +0300

    first push

--- lib/component/git_test.dart
+++ lib/component/git_test.dart
@@ -5,6 +5,6 @@ class GitTest extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('TESSSSSST'),);
+    return const Center(child: Text('first Branch'),);
   }
 }
